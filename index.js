c0b9be36f83f422446b31a8a1b0ec5c86aa02ae6
SajeedAninda <sajaninda@gmail.com>
2024-01-11 22:35:24 +0600

    Created Endpoint to post course details to db

--- index.js
+++ index.js
@@ -14,24 +14,33 @@ const uri = `${process.env.MONGO_URI}`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        const courseCollection = client.db("Scholarix").collection("courses");
+
+        // POST COURSE DETAILED DATA 
+        app.post("/courses", async (req, res) => {
+            let courseData = req.body;
+            let result = await courseCollection.insertOne(courseData);
+            res.send(result);
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
